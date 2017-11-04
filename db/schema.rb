c755365e7201f70fbe74c6ab2167695fd46d200a
Magi Marinova <magi.marinowax@gmail.com>
2017-11-04 17:41:41 +0200

    kill me

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20171104143334) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "messages", force: :cascade do |t|
     t.text "text"
     t.datetime "created_at", null: false
